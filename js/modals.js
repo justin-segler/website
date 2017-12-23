cb4dfd26dde1bf17545ecdbd675948a82636cfc2
justin-segler <justinsegler@justins-mbp.attlocal.net>
2017-12-22 21:01:33 -0600

    modal images

--- js/modals.js
+++ js/modals.js
@@ -1,6 +1,8 @@
 $(document).ready(function(e) {
   //First
   $("#portfolio1Link").click(function() {
+    $('#portfolioModal1').removeClass('closed');
+    $('#portfolioModal1').scrollTop(0);
     $( "#top" ).css("overflow", "hidden");
     $( "#header" ).fadeTo(200 , 0, function() {
       $('#portfolioModal1').css("overflow-y", "scroll");
@@ -14,6 +16,7 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal1 .modal-content').css("height", 0);
+        $('#portfolioModal1').addClass('closed');
       });
     });
   });
@@ -23,12 +26,15 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal1 .modal-content').css("height", 0);
+        $('#portfolioModal1').addClass('closed');
       });
     });
   });
 
   //Second
   $("#portfolio2Link").click(function() {
+    $('#portfolioModal2').removeClass('closed');
+    $('#portfolioModal2').scrollTop(0);
     $( "#top" ).css("overflow", "hidden");
     $( "#header" ).fadeTo(200 , 0, function() {
       $('#portfolioModal2').css("overflow-y", "scroll");
@@ -42,6 +48,7 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal2 .modal-content').css("height", 0);
+        $('#portfolioModal2').addClass('closed');
       });
     });
   });
@@ -51,12 +58,15 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal2 .modal-content').css("height", 0);
+        $('#portfolioModal2').addClass('closed');
       });
     });
   });
 
   //Third
   $("#portfolio3Link").click(function() {
+    $('#portfolioModal3').removeClass('closed');
+    $('#portfolioModal3').scrollTop(0);
     $( "#top" ).css("overflow", "hidden");
     $( "#header" ).fadeTo(200 , 0, function() {
       $('#portfolioModal3').css("overflow-y", "scroll");
@@ -70,6 +80,7 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal3 .modal-content').css("height", 0);
+        $('#portfolioModal3').addClass('closed');
       });
     });
   });
@@ -79,12 +90,15 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal3 .modal-content').css("height", 0);
+        $('#portfolioModal3').addClass('closed');
       });
     });
   });
 
   //Fourth
   $("#portfolio4Link").click(function() {
+    $('#portfolioModal4').removeClass('closed');
+    $('#portfolioModal4').scrollTop(0);
     $( "#top" ).css("overflow", "hidden");
     $( "#header" ).fadeTo(200 , 0, function() {
       $('#portfolioModal4').css("overflow-y", "scroll");
@@ -98,6 +112,7 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal4 .modal-content').css("height", 0);
+        $('#portfolioModal4').addClass('closed');
       });
     });
   });
@@ -107,6 +122,7 @@ $(document).ready(function(e) {
       $( "#header" ).fadeTo(100 , 1, function() {
         $( "#top" ).css("overflow", "scroll");
         $('#portfolioModal4 .modal-content').css("height", 0);
+        $('#portfolioModal4').addClass('closed');
       });
     });
   });
