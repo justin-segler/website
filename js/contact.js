8224fea186f2da4f11611252b3575a8918f6af05
justin-segler <justinsegler@Justins-MBP.attlocal.net>
2018-05-14 15:17:05 -0500

    added contact

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,11 @@
-$(function() {
-
-  $("#contactForm input,#contactForm textarea").onClick(() => {
+$(document).ready(() => {
+  $("#sendMessageButton").click(function(event){
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      var message = name + "\n" + email + "\n" + phone + "\n\n" + $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -14,57 +13,42 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
-          // Success message
+
+      Email.send("noreply@justinsegler.com",
+        "justin@djsegler.com",
+        name,
+        message,
+        "smtp.elasticemail.com",
+        "justin@djsegler.com",
+        "0e21dc62-ccbb-47a1-abb3-d83ebbb3c1b7",
+        function done(m) {
+          event.preventDefault();
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
           $('#success > .alert-success')
             .append("<strong>Your message has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
         }
-      });
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
-  });
+      );
+
+      setTimeout(function() {
+        $("#sendMessageButton").prop("disabled", false); // Re-enable submit button when AJAX call is complete
+      }, 1000);
+
+      setTimeout(function() {
+        $("#success").remove();
+      }, 5000);
+    });
 
   $("a[data-toggle=\"tab\"]").click(function(e) {
     e.preventDefault();
     $(this).tab("show");
   });
-});
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
+  /*When clicking on Full hide fail/success boxes */
+  $('#name').focus(function() {
+    $('#success').html('');
+  });
 });
